bcb9cb3afafb8d3882efa4a48be42e80bfbdb6af
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-10 13:34:59 +0000

    Add files via upload

---
+++ changeColor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
