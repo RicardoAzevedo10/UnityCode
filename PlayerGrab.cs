8250cef44a1bf0d7b3489f8f0902fb290077c365
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-10 13:34:04 +0000

    Add files via upload
    Code in VR for the player grab the object

---
+++ PlayerGrab.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject myHand;
+    public float handPower;
+
+    bool inHands = false;  
+    Collider ballCol;
+    Rigidbody ballRb;
+    Camera cam;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        ballCol = ball.GetComponent<SphereCollider>();
+        ballRb = ball.GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!inHands)
+            {
+                //Grab the ball
+                ballCol.isTrigger = true;
+                ball.transform.SetParent(myHand.transform);
+                ball.transform.localPosition = new Vector3(0f, -0.336f, 0.544f);
+                ballRb.velocity = Vector3.zero;
+                ballRb.useGravity = false;
+                inHands = true;
+               
+
+
+
+            }
+            else if(inHands)
+            {
+                //Realease the ball
+                ballCol.isTrigger = false;
+                ballRb.useGravity = true;
+                this.GetComponent<PlayerGrab>().enabled = false;
+                ball.transform.SetParent(null);
+                ballRb.velocity = cam.transform.rotation * Vector3.forward * handPower;
+                inHands = false;
+               
+
+            }
+          
+
+        }
+    }
+}
