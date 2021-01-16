f958bd7650141e1960d6db1063c960bfc10a7751
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-16 23:52:04 +0000

    Update changeColor.cs

--- changeColor.cs
+++ changeColor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class changeColor : MonoBehaviour
-{
+{ //This script is to change the color of the gameobject , in this case to blue
     public void Blue()
     {
         GetComponent<Renderer>().material.color = Color.blue;
