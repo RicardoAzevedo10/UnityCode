768baf23e4f03f2e445b4e397a07d124f330139e
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-16 23:53:38 +0000

    Update ChangeScene.cs

--- ChangeScene.cs
+++ ChangeScene.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;// Dont forget to call the library "UnityEngine.SceneManagement" 
+                                  //without this you cant acess the parameter
 
 
 public class ChangeScene : MonoBehaviour
-{
+{ //This function is called to change scene 
   public void levelBowling()
     {
         SceneManager.LoadScene("vrbowling");
