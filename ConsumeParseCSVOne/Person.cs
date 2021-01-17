350840e2caf7966c3428cd13bcdec2924e6d1066
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-17 18:13:06 +0000

    Update Person.cs

--- ConsumeParseCSVOne/Person.cs
+++ ConsumeParseCSVOne/Person.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualBasic.CompilerServices;
 namespace ConsumeParseCSVOne
 {
     public class Person
-    {
+    {//This is for the user put the data in this case name,email and country and consume that data
         public string Name { get; set; }
         public string Email { get; set; }
         public string Coutry { get; set; }
