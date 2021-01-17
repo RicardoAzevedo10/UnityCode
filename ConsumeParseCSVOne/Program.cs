9e075d469b6c341304df479f8e570e437c070638
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-17 18:14:44 +0000

    Update Program.cs

--- ConsumeParseCSVOne/Program.cs
+++ ConsumeParseCSVOne/Program.cs
@@ -9,14 +9,16 @@ namespace ConsumeParseCSVOne
     {
         static void Main(string[] args)
         {
+            // Make a array of people
             List<Person> people = new List<Person>();
+            //use streamreader to read all the data and write in .txt format
             using (StreamReader reader = new StreamReader("data.txt"))
             {
                 string line;
             
                 while ((line = reader.ReadLine()) != null)
                 {
-                    string[] tokens = line.Split('|');
+                    string[] tokens = line.Split('|');//make another paragraph
                     people.Add(new Person(tokens[0], tokens[1], tokens[2]));
                 }   
             }
